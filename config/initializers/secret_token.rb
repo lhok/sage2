66faa2f1bb383e8b346db2ce98238280659e3174
lhok <lhok@tom.com>
2012-09-06 15:04:43 +0800

    a

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sage::Application.config.secret_token = 'c0a14e054d9a3a9eae753f9630b296d8b0b3ae8f4a750ce6435b5e5e8062e229d6cf554436443f9301179f3c209ae954fa1a65054725fe8740b25941956cde0c'
